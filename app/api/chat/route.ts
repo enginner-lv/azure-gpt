d3f03043e04f6d825ba33d7b732e270ced9c80ff
liulv <liulv_915@163.com>
2023-07-31 23:54:11 +0800

    pref: api chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,4 @@
 import {Configuration, OpenAIApi} from "openai-edge";
-import {OpenAIStream, StreamingTextResponse} from "ai";
-
-// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.AZURE_OPENAI_API_KEY,
   baseOptions: {
@@ -22,21 +19,40 @@ export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
-    const {messages} = await req.json();
-
-    console.log("messages", messages);
+    const payload = await req.json();
 
     // Ask OpenAI for a streaming completion given the prompt
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      stream: true,
-      messages,
-    });
+    const response = await openai.createChatCompletion(payload);
 
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(response);
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
+    console.log("response.status", response.status);
+    if (payload.stream) {
+      const reader = response?.body?.getReader();
+      const stream = new ReadableStream({
+        start(controller) {
+          function push() {
+            reader
+              ?.read()
+              .then(({done, value}) => {
+                if (done) {
+                  controller.close();
+                  return;
+                }
+                controller.enqueue(value);
+                push();
+              })
+              .catch((error) => {
+                console.error(error);
+                controller.error(error);
+              });
+          }
+          push();
+        },
+      });
+      return new Response(stream);
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {status: 200});
   } catch (error) {
     console.error(error);
 
