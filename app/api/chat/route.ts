906490b0c0c001fffb6b1ba9c0fabd891c2bf529
liulv <liulv_915@163.com>
2023-07-31 18:34:31 +0800

    pref: azure

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,39 +1,60 @@
-import { Configuration, OpenAIApi } from 'openai-edge';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import {Configuration, OpenAIApi} from "openai-edge";
+import {OpenAIStream, StreamingTextResponse} from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.AZURE_OPENAI_API_KEY,
+  baseOptions: {
+    headers: {
+      "api-key": process.env.AZURE_OPENAI_API_KEY,
+    },
+  },
+  basePath: `https://west-europe-01.openai.azure.com/openai/deployments/test01`,
+  defaultQueryParams: new URLSearchParams({
+    "api-version": process.env.AZURE_OPENAI_API_VERSION,
+  } as any),
 });
+
 const openai = new OpenAIApi(config);
 
 // Set the runtime to edge for best performance
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { vibe, bio } = await req.json();
+  try {
+    const {vibe, bio} = await req.json();
 
-  // Ask OpenAI for a streaming completion given the prompt
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      {
-        role: 'user',
-        content: `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
-          vibe === 'Funny'
-            ? "Make sure there is a joke in there and it's a little ridiculous."
-            : null
-        }
+    // Ask OpenAI for a streaming completion given the prompt
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [
+        {
+          role: "user",
+          content: `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
+            vibe === "Funny"
+              ? "Make sure there is a joke in there and it's a little ridiculous."
+              : null
+          }
           Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
-          bio.slice(-1) === '.' ? '' : '.'
-        }`,
-      },
-    ],
-  });
+            bio.slice(-1) === "." ? "" : "."
+          }`,
+        },
+      ],
+    });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+
+    return new Response(JSON.stringify(error), {
+      status: 400,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
 }
